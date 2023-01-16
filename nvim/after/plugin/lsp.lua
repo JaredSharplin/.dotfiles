5d57c029454e4a43452d46b8f816e3fc136b37da
Jared Sharplin <jaredsharplin1@gmail.com>
2023-01-17 00:01:02 +1000

    Add completion mappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,5 +3,17 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({select = true}),
+  ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-e>'] = cmp.mapping.close(),
+})
+
 lsp.setup()
 
