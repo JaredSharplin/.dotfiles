88f92cd914f827a72b0bb43ad2ce18427851b3fd
Jared Sharplin <jaredsharplin1@gmail.com>
2023-01-17 17:10:34 +1000

    Add nvim tree

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = "Open NetRW"})
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {desc = "Open Nvim Tree"})
 vim.keymap.set("n", "<leader>w", ":w<CR>", {desc = "Write"})
 vim.keymap.set("n", "<leader>q", ":q<CR>", {desc = "Quit"})
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", {desc = "Open LazyGit"})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -68,5 +68,13 @@ return require('packer').startup(function(use)
   use('github/copilot.vim')
 
   use("omnisyle/nvim-hidesig")
+
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 end)
 
