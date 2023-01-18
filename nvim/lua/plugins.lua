1ad4f2ba1bd43d3b88edf7a09c000e80d09a6fef
Jared Sharplin <jaredsharplin1@gmail.com>
2023-01-18 14:36:01 +1000

    Add lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -78,5 +78,11 @@ return require('packer').startup(function(use)
   }
 
   use("theprimeagen/harpoon")
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
 end)
 
