7fbd52468e85d626d691e17d55fdb63eb1dbb3d9
Jared Sharplin <jaredsharplin1@gmail.com>
2023-01-20 12:43:37 +1000

    Fix mapping conflict

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,7 +4,7 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file, {desc = "Add File to Harpoon"})
 vim.keymap.set("n", "<C-q>", ui.toggle_quick_menu, {desc = "Toggle Harpoon Quick Menu"})
 
-vim.keymap.set("n", "<C-i>", function() ui.nav_file(1) end, {desc = "Navigate to Harpoon File 1"})
+vim.keymap.set("n", "<C-g>", function() ui.nav_file(1) end, {desc = "Navigate to Harpoon File 1"})
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end, {desc = "Navigate to Harpoon File 2"})
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end, {desc = "Navigate to Harpoon File 3"})
 vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, {desc = "Navigate to Harpoon File 4"})
