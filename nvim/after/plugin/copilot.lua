c0f4111f6f866e6be136f2b012633bf402409386
Jared Sharplin <jaredsharplin1@gmail.com>
2023-01-25 11:17:32 +1000

    Disable copilot in telescope

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,3 +1,6 @@
 vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {
+  ["TelescopePrompt"] = false,
+}
 vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true, desc = "Accept Copilot" })
 
