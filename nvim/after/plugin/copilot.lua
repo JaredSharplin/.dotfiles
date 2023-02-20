04f38a258f8b8a72a3d643b951a291289e74e56c
Jared Sharplin <jaredsharplin1@gmail.com>
2023-02-20 16:50:06 +1000

    Add comment to copilot accept mapping

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -2,5 +2,8 @@ vim.g.copilot_no_tab_map = true
 vim.g.copilot_filetypes = {
   ["TelescopePrompt"] = false,
 }
+
+-- replace_keycodes = false is needed to avoid insertion of weird characters
+-- https://github.com/community/community/discussions/29817#discussioncomment-4217615
 vim.keymap.set("i", "<C-j>", "copilot#Accept('<CR>')", { silent = true, expr = true, replace_keycodes = false, desc = "Accept Copilot" })
 
