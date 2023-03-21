a248fc5688e691d351777d21c7310481d6b957f8
Jared Sharplin <jaredsharplin1@gmail.com>
2023-03-21 13:45:41 +1000

    Fix LSP mappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -27,13 +27,35 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-e>'] = cmp.mapping.close(),
 })
 
-lsp.ensure_installed({
-  'sorbet',
-  'sumneko_lua',
-  'eslint',
+-- cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
 })
 
-lsp.nvim_workspace()
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,  {buffer = bufnr, remap = false, desc = "Goto Definition"})
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, {buffer = bufnr, remap = false, desc = "Hover"})
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, {buffer = bufnr, remap = false, desc = "Workspace Symbol"})
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, {buffer = bufnr, remap = false, desc = "Open Diagnostics"})
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, {buffer = bufnr, remap = false, desc = "Goto Next Diagnostic"})
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, {buffer = bufnr, remap = false, desc = "Goto Previous Diagnostic"})
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, {buffer = bufnr, remap = false, desc = "Code Action"})
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, {buffer = bufnr, remap = false, desc = "References"})
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, {buffer = bufnr, remap = false, desc = "Rename"})
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, {buffer = bufnr, remap = false, desc = "Signature Help"})
+end)
 
 lsp.setup()
 
