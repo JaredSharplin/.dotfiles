ef82ec39552e8eb2056829a397b616e71d372916
Jared Sharplin <jaredsharplin1@gmail.com>
2023-04-02 11:38:18 +1000

    Setup lualine to show filename and git blame

--- nvim/after/plugin/git_blame.lua
+++ nvim/after/plugin/git_blame.lua
@@ -1,12 +1,5 @@
 vim.keymap.set("n", "<leader>gb", ":GitBlameOpenCommitURL<CR>", {desc = "Open Git Blame Commit URL"})
 
-vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
-local git_blame = require('gitblame')
-
-require("lualine").setup {
-  sections = {
-    lualine_c = {
-      { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
-    }
-  }
-}
+vim.g.gitblame_display_virtual_text = 0
+vim.g.gitblame_date_format = "%b %d, %Y"
+vim.g.gitblame_message_template = "<author> • <date>"
--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,33 @@
+local git_blame = require('gitblame')
+
 require("lualine").setup {
   options = {
     theme = "gruvbox-material"
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {
+      {
+        'filename',
+        path = 1
+      },
+      {
+        git_blame.get_current_blame_text,
+        cond = git_blame.is_blame_text_available,
+      },
+    },
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_d = {},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
   },
 }
