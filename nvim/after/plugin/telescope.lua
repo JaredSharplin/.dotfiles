6b421247df81320ba80e57cd8aedd6274c6f5ae6
Jared Sharplin <jaredsharplin1@gmail.com>
2023-11-06 09:25:22 +1000

    Add extra telescope commands

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -6,6 +6,8 @@ vim.keymap.set('n', '<leader>fl', builtin.live_grep, {desc = "Find Live Grep"})
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, {desc = "Find Word Under Cursor"})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Find Buffers"})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = "Find Help Tags"})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {desc = "Find Old Files"})
+vim.keymap.set('n', '<leader>fc', builtin.commands, {desc = "Find Commands"})
 
 require('telescope').setup{
   defaults = {
