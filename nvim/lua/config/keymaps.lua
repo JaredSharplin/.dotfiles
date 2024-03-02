eb81d5f8489244b3c75e2ee891e5abaef7f6bb56
Jared Sharplin <jaredsharplin1@gmail.com>
2024-03-02 16:49:24 +1000

    Add basic keymaps with TODO

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- TODO: Find a an unused mapping for this
+-- vim.keymap.set(
+--   "n",
+--   "<leader>s",
+--   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+--   { desc = "Search and Replace" }
+-- )
+
+vim.keymap.set("n", "<leader>n", ":e ~/programming/payaus/.notes.md<CR>", { desc = "Open Payaus Todo File" })
