dc83aeacd3b2652af765341f2b1cfb212308b028
Jared Sharplin <jaredsharplin1@gmail.com>
2024-03-02 17:27:15 +1000

    Don't need to specify normal mode for keys

--- nvim/lua/plugins/vim_rails.lua
+++ nvim/lua/plugins/vim_rails.lua
@@ -1,6 +1,6 @@
 return {
   "tpope/vim-rails",
   keys = {
-    { "n", "<leader>t", ":A<CR>", desc = "Navigate to Test File" },
+    { "<leader>t", ":A<CR>", desc = "Navigate to Test File" },
   },
 }
