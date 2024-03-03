38ae225a3d94aec5c5e629e27817adee645801fa
Jared Sharplin <jaredsharplin1@gmail.com>
2024-03-04 08:33:07 +1000

    Add clipboard support for neovide

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,3 +11,19 @@
 -- )
 
 vim.keymap.set("n", "<leader>n", ":e ~/programming/payaus/.notes.md<CR>", { desc = "Open Payaus Todo File" })
+
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.keymap.set("t", "<D-v>", '<C-\\><C-n>"+Pi') -- Paste terminal mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
