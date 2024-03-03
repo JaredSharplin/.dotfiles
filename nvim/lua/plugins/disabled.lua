91626ed8f97cbdee3ebf039c9af7a3918c329d27
Jared Sharplin <jaredsharplin1@gmail.com>
2024-03-03 12:25:00 +1000

    Disable bufferline and keymaps

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+    keys = {
+      { "<leader>bp", false },
+      { "<leader>bP", false },
+      { "<leader>bo", false },
+      { "<leader>br", false },
+      { "<leader>bl", false },
+      { "<S-h>", false },
+      { "<S-l>", false },
+      { "[b", false },
+      { "]b", false },
+    },
+  },
+}
