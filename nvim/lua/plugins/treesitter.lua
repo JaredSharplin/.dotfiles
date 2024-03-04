4b88daded65e45388c416eb1e41168602e01ef46
Jared Sharplin <jaredsharplin1@gmail.com>
2024-03-04 13:52:38 +1000

    Actually make nvim hidesig work

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,31 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = { "RRethy/nvim-treesitter-endwise" },
-    opts = function(_, opts)
-      opts.endwise = { enable = true }
-      opts.indent = { enable = true, disable = { "yaml", "ruby" } }
-      opts.ensure_installed = {
-        "bash",
-        "embedded_template",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "ruby",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      }
-    end,
-  },
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = { "RRethy/nvim-treesitter-endwise" },
+  opts = function(_, opts)
+    opts.endwise = { enable = true }
+    opts.indent = { enable = true, disable = { "yaml", "ruby" } }
+    opts.hidesig = {
+      enable = true,
+      opacity = 0.75,
+      delay = 200,
+    }
+    opts.ensure_installed = {
+      "bash",
+      "embedded_template",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "ruby",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    }
+  end,
 }
